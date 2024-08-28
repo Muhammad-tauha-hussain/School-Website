9bb3e027e4a3d9dfd8a02b715261dd0ebce23fca
Muhammad-tauha-hussain <tauhafarooqui083@gmail.com>
2024-08-28 09:49:39 -0700

    completed animation of testimonials section

---
+++ app.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var swiper = new Swiper(".mySwipertestimonails", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    lazy: true, // Enable lazy loading
+  });
+  
